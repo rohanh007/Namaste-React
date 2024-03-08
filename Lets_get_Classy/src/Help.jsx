7b634ed9fcf68eddd751c18bb0b8c253c48d9de7
rohanh007 <77897898+rohanh007@users.noreply.github.com>
2024-03-08 15:48:06 +0530

    fetch help data

--- Lets_get_Classy/src/Help.jsx
+++ Lets_get_Classy/src/Help.jsx
@@ -1,48 +1,71 @@
 import ('../template/css/help.css');
+// import { useEffect } from 'react/cjs/react.production.min';
+import {Help_data} from '../utils/constant';
+import { useEffect, useState } from "react"; 
 const Help = () => {
-
+    const [Faqtitle, setFaqtitle ] = useState(null)
+    useEffect(() => {
+        const helpdata = async () => {
+            try {
+                const data = await fetch(Help_data);
+                if (!data.ok) {
+                    throw new Error(`Http server status:${data.status}`);
+                }
+                const json = await data.json();
+                console.log(json);
+                const issue=json?.data?.issueTypes?.data;
+                console.log(issue);
+                setFaqtitle(issue);
+            } catch(error) {
+                console.error("Http error :", error.message);
+            }
+        }
+        helpdata();
+    }, [])
+    console.log(Faqtitle);
     return (
-        <div class="main-container">
-            <div class="header-section">
-                <div class="sub-header">
-                    <h1 class="main-title">Help & Support</h1>
-                    <div class="tagline">Let's take a step ahead and help you better.</div>
+        <div className="main-container">
+            <div className="header-section">
+                <div className="sub-header">
+                    <h1 className="main-title">Help & Support</h1>
+                    <div className="tagline">Let's take a step ahead and help you better.</div>
                 </div>
             </div>
-            <div class="faq-list">
-                <div class="_2QhOV _3glSS">
-                    <div class="faq">
-                        <div class="faq_head">
+            <div className="faq-list">
+                <div className="_2QhOV _3glSS">
+                    <div className="faq">
+                        <div className="faq_head">
                             <ul>
-                                <li class="fq_tab _1W0l-"><span class="fq_span fmxXC">Partner Onboarding</span></li>
-                                <li class="fq_tab"><span class="fq_span">Legal</span></li>
-                                <li class="fq_tab"><span class="fq_span">FAQs</span></li>
+                               { Faqtitle.map((issue)=>( <li className="fq_tab _1W0l-"  key={issue.type}><span className="fq_span fmxXC">{issue.title}</span></li>))
+                                                               }                               
+                                {/* <li className="fq_tab"><span className="fq_span">Legal</span></li>
+                                <li className="fq_tab"><span className="fq_span">FAQs</span></li> */}
                             </ul>
                         </div>
-                        <div class="que_list">
+                        <div className="que_list">
                             <div>
-                                <div class="heading_faq">Partner Onboarding</div>
+                                <div className="heading_faq">Partner Onboarding</div>
                                 <div>
-                                    <div class="_3rl8Q">
-                                        <button class="faq-button"><span class="faq-label">I want to partner my restaurant with Swiggy</span><span class="dropdown-icon"></span></button>
+                                   <div className="subque">
+                                        <button className="faq-button"><span className="faq-label">I want to partner my restaurant with Swiggy</span><span className="dropdown-icon"></span></button>
                                     </div>
-                                    <div class="_3rl8Q">
-                                        <button class="faq-button"><span class="faq-label">What are the mandatory documents needed to list my restaurant on Swiggy?</span><span class="dropdown-icon"></span></button>
+                                    <div className="subque">
+                                        <button className="faq-button"><span className="faq-label">What are the mandatory documents needed to list my restaurant on Swiggy?</span><span className="dropdown-icon"></span></button>
                                     </div>
-                                    <div class="_3rl8Q">
-                                        <button class="faq-button"><span class="faq-label">After I submit all documents, how long will it take for my restaurant to go live on Swiggy?</span><span class="dropdown-icon"></span></button>
+                                    <div className="subque">
+                                        <button className="faq-button"><span className="faq-label">After I submit all documents, how long will it take for my restaurant to go live on Swiggy?</span><span className="dropdown-icon"></span></button>
                                     </div>
-                                    <div class="_3rl8Q">
-                                        <button class="faq-button"><span class="faq-label">What is this one time Onboarding fees? Do I have to pay for it while registering?</span><span class="dropdown-icon"></span></button>
+                                    <div className="subque">
+                                        <button className="faq-button"><span className="faq-label">What is this one time Onboarding fees? Do I have to pay for it while registering?</span><span className="dropdown-icon"></span></button>
                                     </div>
-                                    <div class="_3rl8Q">
-                                        <button class="faq-button"><span class="faq-label">Who should I contact if I need help & support in getting onboarded?</span><span class="dropdown-icon"></span></button>
+                                    <div className="subque">
+                                        <button className="faq-button"><span className="faq-label">Who should I contact if I need help & support in getting onboarded?</span><span className="dropdown-icon"></span></button>
                                     </div>
-                                    <div class="_3rl8Q">
-                                        <button class="faq-button"><span class="faq-label">How much commission will I be charged by Swiggy?</span><span class="dropdown-icon"></span></button>
+                                    <div className="subque">
+                                        <button className="faq-button"><span className="faq-label">How much commission will I be charged by Swiggy?</span><span className="dropdown-icon"></span></button>
                                     </div>
-                                    <div class="_3rl8Q">
-                                        <button class="faq-button"><span class="faq-label">I don’t have an FSSAI licence for my restaurant. Can it still be onboarded?</span><span class="dropdown-icon"></span></button>
+                                    <div className="subque">
+                                        <button className="faq-button"><span className="faq-label">I don’t have an FSSAI licence for my restaurant. Can it still be onboarded?</span><span className="dropdown-icon"></span></button>
                                     </div>
                                 </div>
                             </div>
