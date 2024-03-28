5f7a0f3094344e0f6db815666b47e8393a5ccb07
rohanh007 <77897898+rohanh007@users.noreply.github.com>
2024-03-28 12:24:10 +0530

    offer

--- Time_for_test/src/Offer.jsx
+++ Time_for_test/src/Offer.jsx
@@ -1,4 +1,38 @@
+import { useEffect, useState } from "react";
+import RestaurantCard from "../src/RestaurantCard";
+import { DATA_API } from "../utils/constant";
 const Offer = () => {
+    const [offerdata, setOfferdata] = useState([])
+
+    useEffect(() => {
+        usefetchdata =async () => {
+            try {
+                const data =await fetch(DATA_API)
+                //console.log(data);
+                if (!data.ok) {
+                    throw new Error(data.status);
+                }
+                const jsondata = await data.json();
+                async function checkJsonData(jsonData) {
+                    for (let i = 0; i < jsonData?.data?.cards.length; i++) {
+                      let checkData = jsondata?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+                      console.log(checkData)
+                      //let offercheck=checkData?.info?.aggregatedDiscountInfoV2;
+                      if (checkData !== undefined) {
+                        return checkData;
+                      }
+                    }
+                  }
+                  const listdata=await checkJsonData(jsondata);
+                  setOfferdata(listdata);
+                console.log(jsondata);
+            } catch (error) {
+               console.log(error)
+            }
+            
+        }
+        usefetchdata();
+    })
     return (
         <div>
             <div id="container-grid-filter-sort" class="StyleWrapper__ContainerGridNoPadding-sc-10ym97a-8 jxkEGY">
@@ -30,9 +64,9 @@ const Offer = () => {
                     </div>
                 </div>
             </div>
-
+            {/* <div><RestaurantCard/></div> */}
         </div>
     );
 }
 
-export default Offer
+export default Offer;
